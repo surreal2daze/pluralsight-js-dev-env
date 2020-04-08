60498de2a0573a058702eb67c483bc338868fcbc
klong guantero <surreal2daze@hotmail.com>
2020-04-08 13:06:54 +0800

    Module 11 Http

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function(req,res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+
+app.get('/users',function(req,res){
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bobgmail.com"},
+    {"id": 2,"firstName":"Bob","lastName":"Smith","email":"bobgmail.com"},
+    {"id": 3,"firstName":"Bob","lastName":"Smith","email":"bobgmail.com"}
+  ]);
+});
+
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
